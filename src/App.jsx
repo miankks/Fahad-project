c66f62aef3df58c82d91ef4c40ba12b8f6feac7d
Sweden <techbilal85@gmail.com>
2025-08-12 16:24:17 +0200

    compoenets start and header section start

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import About from './pages/About';
 import SignIn from './pages/SignIn';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
