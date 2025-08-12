1d934be0d519bbade66763b2c6cca7de8f78d1cf
Sweden <techbilal85@gmail.com>
2025-08-12 16:08:20 +0200

    react-router-dom and routes add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
-    <div>
-      <p className='text-3xl'>Hello world</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
