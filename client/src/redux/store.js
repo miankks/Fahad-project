2313ac3ea01c3ba32ca99a52afdc14e8caf185d9
miankks <bilal.jan085@gmail.com>
2025-08-18 10:43:38 +0200

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
+const rootReducer = combineReducers({
+    user: userReducer
+})
+
+// is setting the name of the key in the local storage, the version and the storage
+const persistConfig = {
+   key: 'root',
+   storage,
+   version: 1,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+// instead of passing user reducer now can pass persistedReducer
 export const store = configureStore({
-    reducer: {
-        user: userReducer
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
     })
 })
+
+// persistor is going to make the store persist
+export const persistor = persistStore(store);
