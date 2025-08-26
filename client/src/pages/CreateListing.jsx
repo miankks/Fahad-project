9ae60d4f6b33bdb257684537d5882ead0d2efebc
miankks <bilal.jan085@gmail.com>
2025-08-26 15:53:48 +0200

    complete show user listings functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -131,7 +131,7 @@ const CreateListing = () => {
             if (data.success === false) {
                 setError(data.message);
             }
-            navigate(`/listing/${data._id}`)
+            // navigate(`/listing/${data._id}`)
 
         } catch (error) {
             setError(error.message);
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { useRef } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
+import { Link } from 'react-router-dom';
 import { 
   updateUserStart, 
   updateUserSuccess, 
@@ -15,7 +16,6 @@ import {
   signOutUserFailure
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom'
 
 // Firebase image store rules
 //allow read;
@@ -30,7 +30,8 @@ const Profile = () => {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess ] = useState(false)
-  
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState({})
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -124,6 +125,21 @@ const Profile = () => {
         dispatch(signOutUserFailure(data.message))
     }
   }
+
+  const handleShowListing = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data)
+    } catch (error) {
+      setShowListingsError(true) 
+    }
+  }
   // for image use self-center in image tag
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -170,6 +186,30 @@ const Profile = () => {
       </div>
       <p className='text-red-700 mt-5'>{error? error : ''}</p>
       <p className='text-green-700 mt-5'>{updateSuccess ? 'User is Updated successfully' : ''}</p>
+      <button onClick={handleShowListing} className='text-green-700 w-full'>Show Listings</button>
+      <p className='text-red-700 mt-5'>
+        {showListingsError ? 'Error showing listing' : ''}
+      </p>
+      {userListings && userListings.length > 0 &&
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+        {userListings.map((listing) => (
+          <div key={listing._id} className='flex border rounded-lg p-3 justify-between items-center gap-4'>
+            <Link to={`/listing/${listing._id}`}>
+              <img src={listing.imageUrls[0]} alt="listing cover" 
+                className='h-16 w-16 object-contain rounded-lg'/>
+            </Link>
+            <Link to={`/listing/${listing._id}`} className=' flex-1 text-slate-700 font-semibold hover:underline truncate'>
+              <p>{listing.name}</p>
+            </Link>
+            <div className="flex flex-col items-center">
+                <button className='text-red-700 uppercase'>Delete</button>
+                <button className='text-green-700 uppercase'>Edit</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      }
     </div>
   )
 }
